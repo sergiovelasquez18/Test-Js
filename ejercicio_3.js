9b8aadf04d5056d9e4c4be32a64f0a79fa2eda47
sergiovelasquez18 <2458@holbertonschool.com>
2021-11-30 22:36:40 -0500

    update

---
+++ ejercicio_3.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const num1 = prompt('Enter the first number')
+const num2 = prompt('Enter the second number')
+
+let suma = parseInt(num1) + parseInt(num2);
+console.log('The answer is: ', suma)
---
+++ ejercicio_4.js
@@ -0,0 +1,4 @@
+const yearBirth = parseInt(prompt('What is your year of birth?'))
+const ageCurrent = 2021
+let Age = parseInt(ageCurrent) - num1;
+console.log('You are ' + Age + ' years old')
